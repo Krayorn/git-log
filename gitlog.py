e3ef9eacd0b9184f41796937c979ccfb3cb451ba
Nathael Arki <nathael.arki@supinternet.fr>
2018-10-09 17:04:15 +0200

    push commit .png

--- gitlog.py
+++ gitlog.py
@@ -8,21 +8,29 @@ import matplotlib.pyplot as plt
 address = sys.argv[1]
 print(address, f'git --git-dir {address}.git\ log')
 
-res = subprocess.check_output(f'git -C {address}.git\ log --pretty=format:"%ad" ', universal_newlines=True)
+res = subprocess.check_output(f'git --git-dir {address}.git\ log --pretty=format:"%ad" ', universal_newlines=True)
 
 date = {}
 
 timeline = np.zeros((53, 7))
+commit = np.zeros((7, 24))
 
 i = 0
 for line in res.splitlines():
     date[i] = datetime.strptime(line, '%a %b %d %X %Y %z')
-    timeline[date[i].isocalendar()[1]][date[i].isocalendar()[2] -1 ] += 1
+
+    calendar = date[i].isocalendar()
+
+    timeline[calendar[1]][calendar[2] -1 ] += 1
+    commit[calendar[2] -1][date[i].hour] += 1
+
     i += 1
 
 
 fig, ax = plt.subplots(1, 1)
 
 plt.imshow(timeline)
-
 fig.savefig('timeline.png')
+
+plt.imshow(commit)
+fig.savefig('commit.png')
